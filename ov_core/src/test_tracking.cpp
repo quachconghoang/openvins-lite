359e08493e026cdda5cc7c4d74ade7922b52dd27
quachconghoang <hoangqc@vnu.edu.vn>
2023-03-13 04:55:10 +1100

    Stucking ... bug init IMU

--- ov_core/src/test_tracking.cpp
+++ ov_core/src/test_tracking.cpp
@@ -96,7 +96,7 @@ int main(int argc, char **argv) {
 
   // Location of the ROS bag we want to read in
   std::string path_to_bag;
-  nh->param<std::string>("path_bag", path_to_bag, "/home/patrick/datasets/euroc_mav/V1_01_easy.bag");
+  nh->param<std::string>("path_bag", path_to_bag, "/home/hoangqc/Datasets/TartanAir_Bag/office_004.bag");
   // nh->param<std::string>("path_bag", path_to_bag, "/home/patrick/datasets/open_vins/aruco_room_01.bag");
   PRINT_INFO("ros bag path is: %s\n", path_to_bag.c_str());
 
